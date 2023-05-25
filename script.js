d594f75507931d5fac69dd81875b7d1f310fdabd
Sanji <sanghrash92@gmail.com>
2023-05-25 18:27:48 +0100

    Finished the game with some help. Need to come back on it

--- script.js
+++ script.js
@@ -1,21 +1,88 @@
-const board = document.querySelector('.board-container');
-const cells = document.querySelectorAll('[data-cell-id]');
-const playerOne = 'X';
-const playerTwo = "O";
-let playersTurn = false;
+const gameboard = (() => {
+    const statusDisplay = document.querySelector('.message-container');
+    const cells = document.querySelectorAll(".cell");
+    const restart = document.querySelector('.game--restart')
 
-cells.forEach(cell => {
-    cell.addEventListener("click", (e) => {
-        if(playersTurn === false) {
-            cell.innerText = playerOne;
-            cell.style.fontSize = '5rem';
-            playersTurn = true;
-        } else {
-            cell.innerText = playerTwo;
-            cell.style.fontSize = '5rem';
-            playersTurn = false;
+    let gameActive = true;
+
+    let playerOne = "X";
+
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
+
+    const handlePlayerChange = () => {
+        playerOne = playerOne === 'X' ? "O" : "X";
+    }
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    const checkWin = () => {
+        let roundWon = false;
+        for (let i = 0; i < winningConditions.length; i++) {
+            const winCondition = winningConditions[i];
+            let a = gameBoard[winCondition[0]];
+            let b = gameBoard[winCondition[1]];
+            let c = gameBoard[winCondition[2]];
+
+            if (a === '' || b === '' || c === '') continue;
+            if (a === b && b === c) {
+                roundWon = true;
+                break;
+            }
         }
-    })
-})
+        
+        if (roundWon) {
+            statusDisplay.innerHTML = `Congrats ${playerOne} won the game!`;
+            gameActive = false;
+            return;
+        }
+
+        let rounDraw = !gameBoard.includes('');
+        if (rounDraw) {
+            statusDisplay.innerHTML = 'Game is a draw!';
+            gameActive = false;
+            return;
+        }
+
+        handlePlayerChange()
+    }
+
+    const handleCellPlayed = (clickedCell, clickedIndex) => {
+        gameBoard[clickedIndex] = playerOne;
+        clickedCell.innerHTML = playerOne;
+        clickedCell.style.fontSize = '6rem'
+    }
+
+    const handleCellClick = e => {
+        const clickedCell = e.target;
+        const clickedIndex = parseInt(
+            clickedCell.getAttribute('data-cell-num')
+        );
+        if(gameBoard[clickedIndex] !== "" || !gameActive) return;
+
+        handleCellPlayed(clickedCell, clickedIndex);
+        checkWin()
+    }
+
+    function handleRestartGame() {
+        gameActive = true;
+        playerOne = "X";
+        gameBoard = ["", "", "", "", "", "", "", "", ""];
+        document.querySelectorAll('.cell')
+                   .forEach(cell => cell.innerHTML = "");
+    }
+  
+
+    cells.forEach(cell => cell.addEventListener("click", handleCellClick));
+    restart.addEventListener('click', handleRestartGame)
+})();
 
 
